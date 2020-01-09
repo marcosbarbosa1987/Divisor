bab69af37fdcc55f27008794d09002d9d01898a6
Marcos Barbosa <marcosbarbos1987@live.com>
2020-01-08 23:42:29 -0300

    Initial Commit

---
+++ divisor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  divisor
+//
+//  Created by Marcos Barbosa on 08/01/20.
+//  Copyright © 2020 Marcos Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

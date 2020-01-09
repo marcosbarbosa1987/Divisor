1279282233f80c51c7ec9f332536b060e06b41c8
Marcos Barbosa <marcosbarbos1987@live.com>
2020-01-09 01:28:33 -0300

    Create first test in completion blocks;

--- divisor/CalculatorBrain.swift
+++ divisor/CalculatorBrain.swift
@@ -10,4 +10,13 @@ import UIKit
 
 class CalculatorBrain: NSObject {
 
+    func divideTwoNumbers(dividend: Int, divisor: Int, completion:((_ result:Float?, _ error:NSError?) -> Void)) {
+        
+        if divisor == 0 {
+            let error = NSError(domain: "Error dividing by Zero", code: 1, userInfo: nil)
+            return completion(nil, error)
+        }
+        
+        return completion(Float(dividend / divisor), nil)
+    }
 }
--- divisorTests/divisorTests.swift
+++ divisorTests/divisorTests.swift
@@ -20,4 +20,17 @@ class divisorTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testTenDivideByFiveMustBeTwo() {
+        calculatorBrain.divideTwoNumbers(dividend: 10, divisor: 5) { (result, error) in
+            XCTAssert(result == 2, "Result must be 2")
+        }
+    }
+    
+    func testTenDivideByZeroMustBeNil() {
+       calculatorBrain.divideTwoNumbers(dividend: 10, divisor: 0) { (result, error) in
+            XCTAssertNil(result, "Result must be nil")
+            XCTAssert(error!.domain == "Error dividing by Zero", "Error message should be 'Error dividing by Zero'")
+        }
+    }
 }
